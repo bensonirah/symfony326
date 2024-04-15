fd455d0fb4765865967b56872bac9b31e1362306
bensonirah <bensonirah@gmail.com>
2024-04-15 16:46:58 +0300

    implement server up command

--- src/handlers/server/up.sh
+++ src/handlers/server/up.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 function server_up() {
-    symfony serve --no-tls -d
+    local _container_name=$(docker ps -a | awk '{print $NF}' | tail -n +2 | fzf)
+    local _container_state=$(docker inspect -f '{{.State.Status}}' $_container_name)
+    if [ "$_container_state" == "running" ]; then
+        symfony serve --no-tls -d
+    else
+        echo "The container $_container_name is down"
+    fi
 }
